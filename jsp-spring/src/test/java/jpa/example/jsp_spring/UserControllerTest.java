14f643512e6826e6ab90ab1283491f77e26eb455
iamai <youngdelta@gmail.com>
2026-01-03 11:08:46 +0900

    feat: Add new Spring REST application with Maven setup and user controller integration tests.

--- jsp-spring/src/test/java/jpa/example/jsp_spring/UserControllerTest.java
+++ jsp-spring/src/test/java/jpa/example/jsp_spring/UserControllerTest.java
@@ -12,10 +12,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+// import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -31,7 +31,7 @@ import jpa.example.jsp_spring.service.UserService;
 
 //@WebMvcTest(UserController.class)
 @SpringBootTest
-@AutoConfigureMockMvc
+// @AutoConfigureMockMvc
 //@ExtendWith(MockitoExtension.class)
 public class UserControllerTest {
 
